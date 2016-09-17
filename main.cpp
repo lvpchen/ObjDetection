cc0349bad68cef7b169c819308f6e1668fd28447
aviravi <aviravi92@gmail.com>
2016-09-17 21:51:52 +0200

    Comments added to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Program to access default camera form an x-86 maschine.
+
 #include"opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 #include<iostream>
@@ -5,16 +7,17 @@
 using namespace cv;
 using namespace std;
 
+// Main function
 int main(){
-  VideoCapture cap(0);
-  if(cap.isOpened()){
+  VideoCapture cap(0); //cap() is used to capture camera  DEFAULT_CAMERA = 0.
+  if(cap.isOpened()){  //returns true if camera is successfully initialized.
   cout<<"Default camera initialized"<<endl;
   }
-Mat frame;
-namedWindow("Camera", WINDOW_AUTOSIZE);
-while(waitKey(30) != 27){
-cap >> frame;
-imshow("Camera", frame);
+Mat frame;  //Creates a Mat object "frame" to hold captured image.
+namedWindow("Camera", WINDOW_AUTOSIZE);  //Creates a window "Camera" of size = AUTOSIZE
+while(waitKey(30) != 27){  // Waits for "esc" key to be pressed.
+cap >> frame;  //captured image is stored in frame.
+imshow("Camera", frame);  // Displays captured image.
 }
   return 0;
 }
